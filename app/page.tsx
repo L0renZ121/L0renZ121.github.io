4664ebbce0b8be8efe6bf4c9442d8b156e32902a
Abhi Khatiwada <khatiwadaabhi123@gmail.com>
2026-02-01 06:51:23 +0545

    feat: Redirect to GitHub profile github.com/AA-maker-dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,5 @@
-'use client'
-
-import Hero from '@/components/sections/Hero'
-import About from '@/components/sections/About'
-import Skills from '@/components/sections/Skills'
-import Projects from '@/components/sections/Projects'
-import Experience from '@/components/sections/Experience'
-import Contact from '@/components/sections/Contact'
-import Navigation from '@/components/Navigation'
-import ParticleBackground from '@/components/ParticleBackground'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <main className="relative">
-      <ParticleBackground />
-      <Navigation />
-      <Hero />
-      <About />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Contact />
-    </main>
-  )
+  redirect('https://github.com/AA-maker-dev')
 }
