ae75e731bf934b08f90ab7d772bb8c2aeacd27e2
Abhi Khatiwada <khatiwadaabhi123@gmail.com>
2026-02-01 07:02:11 +0545

    feat: Restore portfolio page with AA-maker-dev links

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import { redirect } from 'next/navigation'
+'use client'
+
+import Hero from '@/components/sections/Hero'
+import About from '@/components/sections/About'
+import Skills from '@/components/sections/Skills'
+import Projects from '@/components/sections/Projects'
+import Experience from '@/components/sections/Experience'
+import Contact from '@/components/sections/Contact'
+import Navigation from '@/components/Navigation'
+import ParticleBackground from '@/components/ParticleBackground'
 
 export default function Home() {
-  redirect('https://github.com/AA-maker-dev')
+  return (
+    <main className="relative">
+      <ParticleBackground />
+      <Navigation />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Contact />
+    </main>
+  )
 }
